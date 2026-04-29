1ef373afde36d35935d19fd4cf291450a926bb04
IONYX-YOURI <hamzaofficer2@gmail.com>
2026-04-29 02:22:07 +0200

    Add files via upload

---
+++ RUSH_V2/main.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+void rush(int x, int y);
+int main()
+{
+
+    // rush(122, 42);
+    printf("----------------\n");
+    rush(1, 5);
+    printf("----------------\n");
+    rush(5, 1);
+    printf("----------------\n");
+    rush(4, 4);
+    printf("----------------\n");
+    rush(1, 1);
+    printf("----------------\n");
+    return 0;
+}
